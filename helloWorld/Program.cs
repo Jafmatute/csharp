a08c1cd70d203056e97ebeecff62e1cc1fb31c05
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-02-05 11:01:05 -0600

    Type Conversion

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace helloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Variables();
+            TypeConversion();
         }
 
         static void Variables()
@@ -25,6 +25,42 @@ namespace helloWorld
             Console.WriteLine( "{0}, {1}", byte.MinValue, byte.MaxValue);
             Console.WriteLine("{0},{1}", float.MinValue, float.MaxValue);
 
+
+
+        }
+
+        static void TypeConversion()
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine( i );
+
+            //reverse
+            int a = 1;
+            byte ba = (byte) a;
+
+            string number = "1234";
+            int iNumber = Convert.ToInt32(number);
+            Console.WriteLine( iNumber );
+
+            try
+            {
+                var n = "1234";
+                byte bt = Convert.ToByte(n);
+                Console.WriteLine(bt);
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("The number could not be convert to a byte");
+            }
+
+
+            //Boolean
+            var str = "true";
+            bool bs = Convert.ToBoolean(str);
+            Console.WriteLine(bs);
 
 
         }
