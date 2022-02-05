b8370028fa1cbf1a934879336ab2628016f8f133
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-02-05 08:13:23 -0600

    Variables and Constants

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -7,6 +7,26 @@ namespace helloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Variables();
+        }
+
+        static void Variables()
+        {
+            byte number = 1;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Josue";
+            bool isWorking = true;
+
+            const float Pi = 3.14f;
+
+            Console.WriteLine(number);
+            Console.WriteLine( "{0}, {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0},{1}", float.MinValue, float.MaxValue);
+
+
+
         }
     }
 }
