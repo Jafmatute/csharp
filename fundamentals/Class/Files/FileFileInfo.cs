974c826ad56039adeb3ae68ac6950296ee65cb93
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-03-21 10:18:37 -0600

    File and FileInfo

---
+++ fundamentals/Class/Files/FileFileInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace csharpFundamentals.Class.Files
+{
+    class FileFileInfo
+    {
+
+        public void File_()
+        {
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+
+            if (File.Exists(path))
+            {
+                //logic
+
+            }
+
+            var content = File.ReadAllText(path);
+
+
+        }
+
+        public void FileInfo_()
+        {
+            var path = @"c:\somefile.jpg";
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+
+            if (fileInfo.Exists)
+            {
+                //logic
+            }
+
+
+
+
+        }
+    }
+}
