2800b11750df8a615754cb292d19408eb62d86dd
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-03-21 11:04:54 -0600

    Directory and DirectoryInfo

---
+++ fundamentals/Class/Files/DirectoryDirectoryInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace csharpFundamentals.Class.Files
+{
+    class DirectoryDirectoryInfo
+    {
+
+        public void Directory_()
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.sln*", SearchOption.AllDirectories);
+
+            foreach (var file in files) Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\CSharpFundamentals","*.*",SearchOption.AllDirectories);
+
+            foreach (var directory in directories) Console.WriteLine(directory);
+
+            if (Directory.Exists(@"c:\projects\CSharpFundamentals"))
+            {
+                //logic
+            }
+            
+        }
+
+        public void DirectoryInfo_()
+        {
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+
+
+        }
+    }
+}
