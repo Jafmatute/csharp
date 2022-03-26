e6e19bb5649b3947328c08fa9a3880805043bfa3
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-03-26 14:01:12 -0600

    C# Intermediate classes

---
+++ CSharpIntermediate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+
+        }
+
+        public static Person Parse(string str)
+        {
+            Person person = new Person
+            {
+                Name = str
+            };
+            return person;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+           var person = Person.Parse("Josue");
+           person.Introduce("Flores");
+        }
+    }
+}
