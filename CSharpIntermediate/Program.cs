ffb8d830036e667975869edfd7f1a57f356533c4
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-03-26 20:16:38 -0600

    Methods

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Data;
+using System.Globalization;
 using CSharpIntermediate.Classes;
 
 namespace CSharpIntermediate
@@ -10,15 +10,56 @@ namespace CSharpIntermediate
     {
         static void Main()
         {
+            TryParse();
          
-          var customer = new Customer();
-          customer.Id = 1;
-          customer.Name = "Josue";
+        }
 
-          var order = new Order();
-          customer.Orders.Add(order);
-          Console.WriteLine(customer.Id);
-          Console.WriteLine(customer.Name);
+        static void UsePoint()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("an unexpected error ocurred", e);
+                
+            }
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 4 }));
+        }
+
+        static void TryParse()
+        {
+            // method 1
+
+            try
+            {
+                var num = int.Parse("abc");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                
+            }
+            //method 2
+            int number;
+            var result = int.TryParse("abc", out number);
+            if(result) Console.WriteLine(number);
+            else Console.WriteLine("Conversion failed");
         }
     }
 }
